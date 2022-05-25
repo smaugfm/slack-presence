e374cc65bd1cefbcf1bcd5026e4bf1759b477c83
Dmytro Marchuk <dmytro.marchuk@fuib.com>
2022-05-25 08:56:14 +0300

    fix

--- only-chrome.ts
+++ only-chrome.ts
@@ -3,10 +3,7 @@ import { delay } from "./src/util";
 
 (async () => {
   try {
-    const { page } = await createBrowser({
-      headless: true,
-      args: ["--remote-debugging-address=0.0.0.0", "--remote-debugging-port=9222"]
-  });
+    const { page } = await createBrowser("chrome", 9222);
     await page.goto(
       "https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html",
     );
